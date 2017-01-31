1222e6463a42e090f72fb3642956fa6e77813935
Matt Timmons <matt.timmons01@gmail.com>
2017-01-31 13:11:18 -0500

    almost done; just need to get square heights to change.

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-var numsquares = 256;
+var side = 10;
+var row = "";
 $(document).ready(function () {
-    for(i=0;i<=numsquares;i++){
-        $('.container').append('<div class="lilbox"></div>')};
+    draw();
+    setBoxSize;
+    $('#resetBtn').click(function () { // resets the color of all boxes to white. 
+        resetColor();
+    });
+    $('#sizeBtn').click(function () {
+        side = prompt("How many squares should go on each side?")
+        draw();
+    });
 });
+
+var setBoxSize = function () {
+    hw = 400/side;
+    $('.lilbox').css(hw + 'px');
+    $('.lilbox').width(hw + ' px');  
+};
+var resetColor = function () {
+    $('.lilbox').css('background-color', 'white');
+}
+var draw = function () {
+    $('.container').empty();
+    $('.container').append('<table>');
+    row = "";
+    for (var i = 1; i <= side; i++) { //generates table row of 'side' cells and puts in variable "row"
+        row += '<td><div class=lilbox></div></td>'
+    }
+    for (var j = 1; j <= side; j++) { //appends 'side' number of rows to the end of the container
+        $('.container').append('<tr>' + row + '</tr>');
+    };
+    $('.container').append('</table>');
+    setBoxSize();
+
+    $('.lilbox').click(function(){
+        $(this).css('background-color','black');
+    });
+};
