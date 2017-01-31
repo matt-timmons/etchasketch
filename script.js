c09354eb3034fc6ed3b61ad3ce6097de094b7842
Matt Timmons <matt.timmons01@gmail.com>
2017-01-31 14:51:59 -0500

    mark's fixes. notably: .append doesn't need closing tags, don't forget to include parentheses after function calls, define global variables at the beginning, removed spacing in table using css.

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 var side = 10;
+var total = 400;
+var hw = total / side;
 var row = "";
 $(document).ready(function () {
     draw();
-    setBoxSize;
+    setBoxSize();
     $('#resetBtn').click(function () { // resets the color of all boxes to white. 
         resetColor();
     });
     $('#sizeBtn').click(function () {
         side = prompt("How many squares should go on each side?")
         draw();
+        setBoxSize();
     });
 });
 
 var setBoxSize = function () {
-    hw = 400/side;
-    $('.lilbox').css(hw + 'px');
-    $('.lilbox').width(hw + ' px');  
+    hw = total / side;    //hw is height/width of each lilbox. 
+    console.log("height and width are: ", hw);
+    $('.lilbox').css('height', hw + 'px').css('width', hw + 'px');
 };
 var resetColor = function () {
     $('.lilbox').css('background-color', 'white');
 }
 var draw = function () {
     $('.container').empty();
-    $('.container').append('<table>');
-    row = "";
+    var tableEl = $('<table>');
+    $('.container').append(tableEl);
+    var row = "";
     for (var i = 1; i <= side; i++) { //generates table row of 'side' cells and puts in variable "row"
-        row += '<td><div class=lilbox></div></td>'
+        row += '<td><div class="lilbox"></div></td>'
     }
     for (var j = 1; j <= side; j++) { //appends 'side' number of rows to the end of the container
-        $('.container').append('<tr>' + row + '</tr>');
+        tableEl.append('<tr>' + row + '</tr>');
     };
-    $('.container').append('</table>');
     setBoxSize();
 
     $('.lilbox').mouseover(function(){
